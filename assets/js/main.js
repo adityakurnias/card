bccf98765296c6e9f97bfa99b48e3a91514dc6ad
adityakurnias <adityakurniasaputra903@gmail.com>
2025-08-31 21:34:13 +0700

    feat: hover effect

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const card = document.querySelector(".card");
+  const socialIcons = document.querySelectorAll(".social-icon");
+  const websiteBtn = document.querySelector(".website-btn");
+  const characterImg = document.querySelector(".character-img");
+
+  card.addEventListener("mousemove", function (e) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (y - centerY) / 25;
+    const rotateY = (centerX - x) / 25;
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(10px)`;
+  });
+
+  card.addEventListener("mouseleave", function () {
+    card.style.transform =
+      "perspective(1000px) rotateX(0deg) rotateY(0deg) translateZ(0px)";
+  });
+});
